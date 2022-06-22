e869ed750a65e22fdea30a3dc2be8adcb5f66368
hee2425 <bona366@naver.com>
2022-06-22 10:04:18 +0900

    dp(bottom-up)

--- Baekjoon/src/Bj1463.java
+++ Baekjoon/src/Bj1463.java
@@ -21,16 +21,13 @@ public class Bj1463 {
 		dp[0] = dp[1] = 0;  //�ʱ갪
 		
 		for(int i=2; i<=X; i++) {
-			dp[i] = dp[i-1]+1;  // dp[10] = dp[9]+1
-								//dp[2] = dp[1]+1
+			dp[i] = dp[i-1]+1; 	//dp[2] = dp[1]+1
 			
 			if(i%2 == 0) {
 				dp[i] = Math.min(dp[i],  dp[i/2]+1); 
 			}
 			if(i%3 == 0) {
 				dp[i] = Math.min(dp[i],  dp[i/3]+1);
-				//dp[9] = dp[3]+1 
-				//dp[3] = dp[1]+1
 			}
 			
 		}
