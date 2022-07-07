97d1d8b449de47d3a491f394397ed5170d67a02e
hee2425 <bona366@naver.com>
2022-07-07 20:41:52 +0900

    불기->서기

---
+++ Baekjoon/src/Bj18108.java
@@ -0,0 +1,11 @@
+/*불기연도->서기연도*/
+import java.util.Scanner;
+public class Bj18108 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		System.out.println(year-543);
+	}
+
+}
