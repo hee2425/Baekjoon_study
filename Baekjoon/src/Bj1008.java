659cefef4c1809ceb57322abf9f2b98528a16b7c
hee2425 <bona366@naver.com>
2022-07-07 20:27:43 +0900

    a/b입출력

---
+++ Baekjoon/src/Bj1008.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Bj1008 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		
+		double c = a/b;
+		System.out.println(c);
+	}
+
+}
