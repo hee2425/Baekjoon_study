f38e760257928427e786316b1aa57cabceaec4ac
hee2425 <bona366@naver.com>
2022-07-28 15:05:51 +0900

    arrays.sort(x) / stringBuilder

---
+++ Baekjoon/src/Bj2751.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Bj2751 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int n = sc.nextInt();
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0; i<n; i++) {
+			list.add(sc.nextInt());
+		}
+		
+		Collections.sort(list);
+		
+		for(int v:list) {
+			sb.append(v).append("\n");
+		}
+		System.out.println(sb);
+		
+	}
+}
