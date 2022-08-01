c06c216176c11afd158d08edc89b5fc23dc06968
hee2425 <bona366@naver.com>
2022-08-01 14:32:06 +0900

    이분탐색

---
+++ Baekjoon/src/Bj10816.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bj10816 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);	// 이분 탐색을 하기 위해서 정렬.
+
+        int M = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < M; i++) {
+            int target = sc.nextInt();
+            sb.append(upperBound(arr, target) - lowerBound(arr, target)).append(' ');
+        }
+        System.out.println(sb);
+    }
+    private static int lowerBound(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (target <= arr[mid]) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    private static int upperBound(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (target < arr[mid]) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
