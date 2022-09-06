706a1d4a104e7dfe5434472bb51ab500243263e5
hee2425 <bona366@naver.com>
2022-09-06 09:17:21 +0900

    [Doit]평균구하기

---
+++ Baekjoon/src/Bj1546.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Bj1546 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] array = new int[n];
+		for(int i=0; i<n; i++) {
+			array[i] = sc.nextInt();
+		}
+		long sum = 0;
+		long max = 0;
+		for(int i=0; i<n; i++) {
+			if(array[i]>max) max = array[i];
+			sum += array[i];
+		}
+		System.out.println((sum*100.0)/max/3);
+	}
+
+}
