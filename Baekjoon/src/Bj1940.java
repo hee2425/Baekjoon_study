5deb3723399884c3572569a43fbfad17fc557721
hee2425 <bona366@naver.com>
2022-09-19 11:35:50 +0900

    [Doit]투포인터_주몽

---
+++ Baekjoon/src/Bj1940.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj1940 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		int count = 0;
+		int i = 0;
+		int j = n-1;
+		while(i<j) {
+			if(arr[i]+arr[j]<m) {
+				i++;
+			}else if(arr[i]+arr[j]>m) {
+				j--;
+			}else {
+				i++;
+				j--;
+				count++;
+			}
+		}
+		System.out.println(count);
+		br.close();
+		
+		
+		
+	}
+
+}
