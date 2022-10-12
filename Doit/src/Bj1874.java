11e862f2de083e0ec28861bb26462da407ce976b
hee2425 <bona366@naver.com>
2022-10-12 18:06:46 +0900

    [Doit]스택_스택수열

---
+++ Doit/src/Bj1874.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Bj1874 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] A = new int[n];
+		for(int i = 0; i<n; i++) {
+			A[i] = sc.nextInt();
+		}
+		Stack<Integer> stack = new Stack<>();
+		
+		StringBuffer bf = new StringBuffer();
+		int num = 1;
+		boolean result = true;
+		for(int i=0; i<A.length; i++) {
+			int su = A[i];
+			if(su>= num) {
+				while(su >= num) {
+					stack.push(num++);
+					bf.append("+\n");
+				}
+				stack.pop();
+				bf.append("-\n");
+			}
+			else {
+				int N = stack.pop();
+				if(N>su) {
+					System.out.println("NO");
+					result = false;
+					break;
+				}else {
+					bf.append("-\n");
+				}
+				
+			}
+		}
+		if(result) System.out.println(bf.toString());
+	}
+
+}
